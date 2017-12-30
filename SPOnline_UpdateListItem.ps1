52941179162af6336e7cea13dc676296b01e2cad
Sanjay Kumar Pathak <sanjay0778@gmail.com>
2017-12-30 16:14:30 -0500

    Update list item in SP Online
    Script to update list item in Office 365 (SP online)

---
+++ SPOnline_UpdateListItem.ps1
@@ -0,0 +1,22 @@
+﻿#SharePoint Online : CSOM : PowerShell : Update List Item
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+
+$siteURL = ""
+$userId = ""
+$pwd = Read-Host -Prompt "Enter password" -AsSecureString
+$creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($userId, $pwd)
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($siteURL)
+$ctx.credentials = $creds
+try{
+    $lists = $ctx.web.Lists
+    $list = $lists.GetByTitle("TestList")
+    $listItem = $list.GetItemById(1)
+    $listItem["Title"] = "aa"
+    $listItem.Update()
+    $ctx.load($listItem)   
+    $ctx.executeQuery()
+}
+catch{
+    write-host "$($_.Exception.Message)" -foregroundcolor red
+} 
