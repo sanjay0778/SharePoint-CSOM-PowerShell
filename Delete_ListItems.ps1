e7df7d41b995f90e8a60f98d6979d907165b70e5
Sanjay Kumar Pathak <sanjay0778@gmail.com>
2022-08-10 19:07:23 +0530

    Delete SP Online List items in batch of 100
    Script to delete SP Online List items in batch of 100 items at a time

---
+++ Delete_ListItems.ps1
@@ -0,0 +1,12 @@
+﻿#Set Variables
+$SiteURL = "https://abc.sharepoint.com/sites/TestSite/subsite1"
+
+ 
+#Connect to PNP Online
+Connect-PnPOnline -Url $SiteURL -Interactive 
+
+$ListName = "TestList"
+Get-PnPList -Identity $ListName | Get-PnPListItem -PageSize 100 -ScriptBlock { 
+    Param($items) Invoke-PnPQuery } | ForEach-Object {$_.Recycle()
+}
+
